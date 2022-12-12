ee68447ac6479d36d3b6952402662bf4d9a974d0
Caio Beneton <caio.beneton@gmail.com>
2022-12-12 10:13:40 -0300

    feat: get rentals

--- src/controllers/rent.controller.js
+++ src/controllers/rent.controller.js
@@ -1,7 +1,64 @@
 import connection from "../database/database.js"
 import dayjs from "dayjs"
 
-export async function getRentals(req, res) { }
+export async function getRentals(req, res) {
+    const { gameId, customerId } = req.query;
+
+    try {
+        if (!gameId && !customerId) {
+            const rent = await connection.query(`
+                SELECT rentals.*, JSON_BUILD_OBJECT('id',customers.id, 
+                'name', customers.name) AS customer,
+                JSON_BUILD_OBJECT('id',games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name) AS game FROM 
+                rentals JOIN customers ON customers.id = "customerId" JOIN games ON games.id = "gameId"
+                JOIN categories ON categories.id = games."categoryId";
+            `)
+
+            return res.send(rent.rows)
+        } else if (gameId && !customerId) {
+            const rent = await connection.query(`
+                SELECT rentals.*, JSON_BUILD_OBJECT('id',customers.id, 
+                'name', customers.name) AS customer,
+                JSON_BUILD_OBJECT('id',games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name) AS game FROM 
+                rentals JOIN customers ON customers.id = "customerId" JOIN games ON games.id = "gameId"
+                JOIN categories ON categories.id = games."categoryId" WHERE "gameId" = $1;
+            `, [gameId])
+            return res.send(rent.rows)
+        } else if (!gameId && customerId) {
+            const rent = await connection.query(`
+                SELECT rentals.*, JSON_BUILD_OBJECT('id',customers.id, 
+                'name', customers.name) AS customer,
+                JSON_BUILD_OBJECT('id',games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name) AS game FROM 
+                rentals JOIN customers ON customers.id = "customerId" JOIN games ON games.id = "gameId"
+                JOIN categories ON categories.id = games."categoryId" WHERE "customerId" = $1;
+            `, [customerId])
+            return res.send(rent.rows)
+        } else if (gameId && customerId) {
+            const rent = await connection.query(`
+                SELECT rentals.*, JSON_BUILD_OBJECT('id',customers.id, 
+                'name', customers.name) AS customer,
+                JSON_BUILD_OBJECT('id',games.id,
+                'name', games.name,
+                'categoryId', games."categoryId",
+                'categoryName', categories.name) AS game FROM 
+                rentals JOIN customers ON customers.id = "customerId" JOIN games ON games.id = "gameId"
+                JOIN categories ON categories.id = games."categoryId" WHERE "customerId" = $1 AND "gameId" = $2;
+            `, [customerId, gameId])
+            return res.send(rent.rows)
+        }
+    } catch (error) {
+        res.sendStatus(500)
+    }
+}
 
 export async function postRental(req, res) {
     const rent = req.body
